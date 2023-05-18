2ef0fa9f6057fbaf1d5e9b77dffc40a85180e3d6
MuhammadSaikat25 <smsaikat000@gmail.com>
2023-05-18 07:15:53 +0600

    added carousle

---
+++ src/Component/Header.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import header1 from '../assets/header-1.webp'
+import header3 from '../assets/Header-3.webp'
+
+const Header = () => {
+    return (
+        <div>
+            <div className="carousel w-full h-[600px]">
+                <div id="slide1" className="carousel-item relative w-full">
+                    <img  src={header1} className="object-cover w-full" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide4" className="btn btn-circle">❮</a>
+                        <a href="#slide2" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide2" className="carousel-item relative w-full">
+                    <img src={header3} className="w-full object-cover" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide1" className="btn btn-circle">❮</a>
+                        <a href="#slide3" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Nav from './Nav';
 import { Outlet } from 'react-router-dom';
+import Header from './Header';
 
 const Home = () => {
     return (
         <div>
             <Nav></Nav>
+            <Header></Header>
             <Outlet></Outlet>
         </div>
     );
